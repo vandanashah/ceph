f6b330d2dd8172c5d7b63abe13864c5009bf8460
Mihir <mihir@mihir-debian.sa3.in>
2015-08-04 16:26:43 +0530

    Global dedup

--- src/common/ceph_hash.cc
+++ src/common/ceph_hash.cc
@@ -91,14 +91,45 @@ unsigned ceph_str_hash_linux(const char *str, unsigned length)
 	return hash;
 }
 
+static unsigned ceph_str_hash_direct(const char *s, unsigned len)
+{
+#define NUM_PG_BITS (8)
+#define PG_START_BIT (24)
+#define PG_BIT_MASK (((1ULL << NUM_PG_BITS) - 1) << PG_START_BIT)
+
+	unsigned long pvbn = 0, val = 0;
+	char *saveptr;
+	char *aggr, *pvbn_str;
+	char ss[1024];
+
+	strcpy(ss, s);
+
+	aggr = strtok_r(ss, ".", &saveptr);
+	if (!aggr) {
+		return 0;
+	}
+
+	pvbn_str = strtok_r(NULL, ".", &saveptr);
+	if (!pvbn_str) {
+		return 0;
+	}
+
+	pvbn = strtoull(pvbn_str, NULL, 0);
+	val = pvbn & PG_BIT_MASK;
+	val = val >> PG_START_BIT;
+	return (unsigned) val;
+}
+
 
 unsigned ceph_str_hash(int type, const char *s, unsigned len)
 {
 	switch (type) {
 	case CEPH_STR_HASH_LINUX:
-		return ceph_str_hash_linux(s, len);
+		//return ceph_str_hash_linux(s, len);
+		return ceph_str_hash_direct(s, len);
 	case CEPH_STR_HASH_RJENKINS:
-		return ceph_str_hash_rjenkins(s, len);
+		//return ceph_str_hash_rjenkins(s, len);
+		return ceph_str_hash_direct(s, len);
 	default:
 		return -1;
 	}
